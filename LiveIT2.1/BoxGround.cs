c157526d98d59a633cf9ff5940b546a35f70a1a1
Van Palamar <rami.morri@gmail.com>
2014-11-07 10:22:50 +0100

    texture

--- LiveIT2.1/BoxGround.cs
+++ LiveIT2.1/BoxGround.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,11 @@ namespace LiveIT2._1
 {
     public enum BoxGround
     {
-        None,
-        Grass,
-        Snow,
-        Desert,
-        Water,
-        Forest
-    }
+        None = 0,
+        Grass = 1,
+        Snow = 2,
+        Desert = 3,
+        Water = 4,
+        Forest = 5
+    }  
 }
---
+++ LiveIT2.1/Texture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveIT2._1
+{
+    public class Texture
+    {
+
+        public Bitmap SetTexture(Box box)
+        {
+            return new Bitmap(@"..\..\..\assets\" + box.Ground.ToString()+".jpg");
+        }
+    }
+}
