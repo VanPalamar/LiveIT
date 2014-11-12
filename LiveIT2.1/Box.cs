6d1489b8ea1894f0f89bb2f92800149ded4612d9
val92130 <chatelain@intechinfo.fr>
2014-11-12 16:27:27 +0100

    swc

--- LiveIT2.1/Box.cs
+++ LiveIT2.1/Box.cs
@@ -85,7 +85,6 @@ namespace LiveIT2._1
         internal void Draw( Graphics g, Rectangle target, Texture textures )
         {
             g.DrawImage(textures.LoadTexture(this), new Rectangle( this.Area.X, this.Area.Y, this.Source.Width, this.Source.Height ) );
-
         }
         
     }
